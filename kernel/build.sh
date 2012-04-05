358782ffc50124304f0a41b8c8c77666bafb385c
Sarthak Acharya <sa6915@gmail.com>
2012-04-05 14:31:28 +0530

    block: Add V(R) and Sio(default) I/O Schedulers;Enable CIFS module Support;Disable ext3

--- kernel/build.sh
+++ kernel/build.sh
@@ -8,16 +8,17 @@ make cranium_defconfig
 cp current_version .version
 
 # Make zImage
-make -j8
+make -j48
 
 # Copy output files
 mv arch/arm/boot/zImage boot.img-zImage
 cp boot.img-zImage /home/sarthak/Documents/kernel-htc-pico/boot.img-tools/unpack	
 cp drivers/net/wireless/bcm4330b2/bcm4330.ko /home/sarthak/Documents/kernel-htc-pico/boot.img-tools/output/system/lib/modules
 cp drivers/net/kineto_gan.ko /home/sarthak/Documents/kernel-htc-pico/boot.img-tools/output/system/lib/modules
+cp fs/cifs/cifs.ko /home/sarthak/Documents/kernel-htc-pico/boot.img-tools/output/system/lib/modules
+rm boot.img-zImage
 cd ../
 cd boot.img-tools
-rm target_img/boot.img
 
 # Make boot.img
 tools/mkbootfs boot | gzip > unpack/boot.img-ramdisk-new.gz
@@ -30,7 +31,7 @@ cp target_img/boot.img output
 cd output 
 
 #Make CWM-flashable zip 
-zip cranium-kernel.zip boot.img META-INF/com/google/android/updater-script META-INF/com/google/android/update-binary system/lib/modules/bcm4330.ko system/lib/modules/kineto_gan.ko
+zip cranium-kernel.zip boot.img META-INF/com/google/android/updater-script META-INF/com/google/android/update-binary system/lib/modules/bcm4330.ko  system/lib/modules/kineto_gan.ko system/lib/modules/cifs.ko
 cp cranium-kernel.zip /home/sarthak/Documents/kernel-htc-pico/auto-sign
 cd ../../
 
