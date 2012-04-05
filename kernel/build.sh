6a5c1fdd6884422d3248cc17a568c9ed675f0cd1
Sarthak Acharya <sa6915@gmail.com>
2012-04-05 07:51:01 +0530

    Add Auto-CWM Build and Auto-signer

--- kernel/build.sh
+++ kernel/build.sh
@@ -6,8 +6,8 @@ make -j8
 # Copy output files
 mv arch/arm/boot/zImage boot.img-zImage
 cp boot.img-zImage /home/sarthak/Documents/kernel-htc-pico/boot.img-tools/unpack	
-cp drivers/net/wireless/bcm4330b2/bcm4330.ko /home/sarthak/Documents/kernel-htc-pico/boot.img-tools/output
-cp drivers/net/kineto_gan.ko /home/sarthak/Documents/kernel-htc-pico/boot.img-tools/output
+cp drivers/net/wireless/bcm4330b2/bcm4330.ko /home/sarthak/Documents/kernel-htc-pico/boot.img-tools/output/system/lib/modules
+cp drivers/net/kineto_gan.ko /home/sarthak/Documents/kernel-htc-pico/boot.img-tools/output/system/lib/modules
 cd ../
 cd boot.img-tools
 rm target_img/boot.img
@@ -17,9 +17,16 @@ tools/mkbootfs boot | gzip > unpack/boot.img-ramdisk-new.gz
 mkdir -p target_img
 tools/mkbootimg --kernel unpack/boot.img-zImage --ramdisk unpack/boot.img-ramdisk-new.gz -o target_img/boot.img --base `cat unpack/boot.img-base`	
 cp target_img/boot.img output
+
 #Strip modules
 ./strip.sh
+cd output 
 
-#Profit!
+#Make cwm zip and sign
+zip cranium-kernel.zip boot.img META-INF/com/google/android/updater-script META-INF/com/google/android/update-binary system/lib/modules/bcm4330.ko system/lib/modules/kineto_gan.ko
+cp cranium-kernel.zip /home/sarthak/Documents/kernel-htc-pico/auto-sign
+cd ../../
+cd auto-sign
+java -jar signapk.jar testkey.x509.pem testkey.pk8 cranium-kernel.zip signed_cranium-kernel.zip
 
 
