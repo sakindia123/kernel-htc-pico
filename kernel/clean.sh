ce98179e42e272b81c51d3bd3f44bf5ddce01939
Sarthak Acharya <sa6915@gmail.com>
2012-05-04 21:14:49 +0530

    Disable even more CRAP

--- kernel/clean.sh
+++ kernel/clean.sh
@@ -1,3 +1,21 @@
+echo  "========================================================= "
+echo  "         CRANIUM KERNEL CLEANING SCRIPT                   "
+echo " ========================================================= "
+
+
 rm .config
 make clean -j32
+cd ../
+cd auto-sign && rm signed_cranium-kernel.zip
+cd ../
+cd boot.img-tools/output
+rm cranium-kernel.zip boot.img
+cd system/lib/modules
+rm *.ko
+cd ../../../../
+cd target_img && rm boot.img
 
+
+echo  "========================================================= "
+echo  "         CRANIUM KERNEL CLEANED!!                         "
+echo " ========================================================= "
