62dc9f3e4fd3173f6bae8d9d6f422ddacbe7c859
Sarthak Acharya <sa6915@gmail.com>
2012-05-09 09:59:33 +0530

    drivers: mmc: core: Set suspend/resume bus operations if CONFIG_PM_RUNTIME is used

--- boot.img-tools/strip.sh
+++ boot.img-tools/strip.sh
@@ -1,6 +1,12 @@
+#Change to corresponding path to the binary of your toolchain!
+
+echo " ---------  CRANIUM KERNEL MODULE STRIPPER FOR ARM-EABI-4.4.3 ---------- "
+
 for i in $(find . | grep .ko | grep './')
 do
         echo $i
 /media/Main_Storage/android_toolchains/arm-eabi-4.4.3/bin/arm-eabi-strip --strip-unneeded $i
 
 done
+
+echo " --------  MODULES STRIPPED! --------- "
